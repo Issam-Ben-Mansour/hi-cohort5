b4e6df3ef8ed888f87eda12177ddcd7a8191366c
Issam-Ben-Mansour <issambenmansour@hotmail.com>
2020-06-15 11:00:50 +0100

    warm-up 1

--- warm-up/warm-up.js
+++ warm-up/warm-up.js
@@ -1,3 +1,30 @@
-function sum(numberOne, numberTwo){
-	return numberTwo + numberOne ;
+// 1-using + operator combine your partner first and last name .
+// 2-find if the number 13 is a multiple of 3 or not.
+// 4-calculate your age in seconds.
+// your code is here
+
+1.
+function name (firstName,lastName){
+
+	return "fistName "+"lastName"
 }
+
+2.
+function multiple (a){
+
+	 if (a%3 !== 0) 
+	 return true
+	 else  
+	 return false
+
+}
+
+
+4.
+ function age (a){
+	return a*365*24*3600
+
+
+}
+
+
